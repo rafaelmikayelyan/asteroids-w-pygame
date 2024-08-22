6cb0656c6a9225cf627a4fa7e960e698c425c3f0
rafaelmikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2024-08-22 12:35:24 -0700

    feat: add player movement

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
--- player.py
+++ player.py
@@ -27,6 +27,10 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -35,3 +39,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_RIGHT]:
             self.rotate(dt)
+        if keys[pygame.K_UP]:
+            self.move(dt)
+        if keys[pygame.K_DOWN]:
+            self.move(-dt)
